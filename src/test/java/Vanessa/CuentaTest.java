db9ea5dfa0783d7e7375521e498608c696abf4fd
Vanessa <vballeste18@gmail.com>
2017-06-21 14:03:38 -0300

    primer commit

---
+++ src/test/java/Vanessa/CuentaTest.java
@@ -0,0 +1,14 @@
+package Vanessa;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CuentaTest {
+
+	@Test
+	public void test() {
+		Cuenta cuenta = new Cuenta(10, 120, "Vanessa");
+	}
+
+}
