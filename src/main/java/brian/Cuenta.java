16e570acf2fcd2aeee4b1dcbff721153839659b0
jferrino <jpferrino@gmail.com>
2017-06-21 13:48:30 -0300

    Correccion de mi primer commit

--- src/main/java/brian/Cuenta.java
+++ src/main/java/brian/Cuenta.java
@@ -2,19 +2,33 @@ package brian;
 
 public class Cuenta {
 	protected long id;
-	protected long cantidadDepositada;
+	protected long saldo;
 	protected String nombreUsuario;
-	
+
 	public long getId() {
 		return id;
 	}
-	
+
 	public void setId(long id) {
 		this.id = id;
 	}
-	
-	public Cuenta(long id, String nombreUsuario){
+
+	public Cuenta(long id, String nombreUsuario) {
 		this.id = id;
 		this.nombreUsuario = nombreUsuario;
+		this.saldo = 0;
+	}
+
+	public void retirar(long cantidad) {
+		this.saldo -= (this.saldo >= cantidad) ? cantidad : 0;
+	}
+
+	public void depositar(long cantidad) {
+		this.saldo += cantidad;
+	}
+	
+	public long getSaldo()
+	{
+		return this.saldo;
 	}
 }
