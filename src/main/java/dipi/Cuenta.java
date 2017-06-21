04ebc2436eef2adcf07ad721dbaf1fcdb5bf1992
Nicolas D'Ippolito <ndippolito@dc.uba.ar>
2017-06-21 11:41:56 -0300

    nueva version

---
+++ src/main/java/dipi/Cuenta.java
@@ -0,0 +1,12 @@
+package dipi;
+
+public class Cuenta {
+	
+	long id;
+	
+	public Cuenta(long id) {
+		this.id = id;
+	}
+	
+	
+}
---
+++ src/test/java/dipi/CuentaTest.java
@@ -0,0 +1,14 @@
+package dipi;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CuentaTest {
+
+	@Test
+	public void testCrearUnaCuenta() {
+		Cuenta cuenta = new Cuenta(10);
+	}
+
+}
