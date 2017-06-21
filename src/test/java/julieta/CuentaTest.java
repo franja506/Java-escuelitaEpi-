af89552ce276b52edec332b862e4256b6903f650
hernanfiuba <hernanfiuba2009@gmail.com>
2017-06-21 13:46:50 -0300

    julieta test

--- src/test/java/julieta/CuentaTest.java
+++ src/test/java/julieta/CuentaTest.java
@@ -10,6 +10,7 @@ public class CuentaTest {
 	public void testDepositarEnCuenta() {
 		Cuenta nuevaCuenta = new Cuenta(111);   //id de la cuenta
 		nuevaCuenta.depositar(100);				//deposito en la cuenta 100
+		assertTrue(nuevaCuenta.verSaldo() == 100);
 	}
 	
 }
