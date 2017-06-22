b7fc1cf6af1693d4782d7847f3b1dfe2cc278a07
Vanessa <vballeste18@gmail.com>
2017-06-22 14:09:50 -0300

    Agregada cuenta corriente y tests

--- src/main/java/vanessa/Cuenta.java
+++ src/main/java/vanessa/Cuenta.java
@@ -1,31 +1,50 @@
 package vanessa;
+import dipi.ICuenta;
 
-public class Cuenta {
+public class Cuenta implements ICuenta {
 	long id;
-	double saldo;
+	long saldo;
 	String nombreCliente;
 	
-	public Cuenta(long id, double saldo, String nombreCliente){
+	public Cuenta(long id, String nombreCliente){
 		this.id = id;
-		this.saldo = saldo;
+		this.saldo = 0;
 		this.nombreCliente = nombreCliente;
 	};
 	
-	public long GetId(){
+
+	@Override
+	public long getId() {
 		return this.id;
-	};
+	}
 	
-	public double VerSaldo(){
+	@Override
+	public long getSaldo() {
 		return this.saldo;
-	};
+	}; 
 	
-	public void Depositar(double nSaldo){
-		this.saldo = this.saldo + nSaldo;
-	};
-	
-	public void Extraer(double nSaldo){
-		this.saldo = this.saldo - nSaldo;
-	};
-	
+	@Override
+	public String getNombreCliente() {
+		return this.nombreCliente;
+	}
+
+	@Override
+	public boolean depositar(long montoADepositar) {
+		if (montoADepositar > 0){
+			this.saldo = this.saldo + montoADepositar;
+			return true;
+		}else
+			return false;
+	}
+
+	@Override
+	public boolean extraer(long montoAExtraer) {
+		if (montoAExtraer > 0 && (this.saldo >= montoAExtraer)){
+			this.saldo = this.saldo - montoAExtraer;
+			return true;
+		}else
+			return false;
+	}
+		
 
 }
--- src/test/java/vanessa/CuentaTest.java
+++ src/test/java/vanessa/CuentaTest.java
@@ -9,8 +9,25 @@ import vanessa.Cuenta;
 public class CuentaTest {
 
 	@Test
-	public void test() {
-		Cuenta cuenta = new Cuenta(10, 120, "Vanessa");
+	public void testDepositosCuenta() {
+		Cuenta c = new Cuenta(1, "Vanessa Balleste");
+		boolean depositar = c.depositar(50);
+		assertTrue(depositar);
+		assertEquals(50, c.getSaldo());
+		
+		depositar = c.depositar(-50);
+		assertFalse(depositar);
+		assertEquals(50, c.getSaldo());
 	}
-
+	
+	@Test
+	public void testExtraccionMayorAlSaldoCuenta() {
+		Cuenta c = new Cuenta(1, "Vanessa Balleste");
+		c.depositar(50);
+		boolean extraer = c.extraer(60);
+		assertFalse(extraer);
+		assertEquals(50, c.getSaldo());
+	}
+		
+	
 }
