c31c0dee5a20b4241c9a548175dfe0812d27e8b9
Your Name <you@example.com>
2017-06-22 14:08:17 -0300

    un poco del test

--- src/test/java/ivan/CuentaTest.java
+++ src/test/java/ivan/CuentaTest.java
@@ -8,16 +8,13 @@ public class CuentaTest {
 
 	@Test
 	public void ExtraerNegativotest() {
-		/**	int nro = 1234;
-			String cliente= "ivan delcuadri";
-			Cuenta cuenta = new Cuenta(nro, cliente);
-			System.out.println("la cuenta creada tiene el numero: " + cuenta.getId() +" y el nombre: " + cuenta.getNombreCliente() );
-			
-			
-			System.out.println("la cuenta tiene el saldo" + cuenta.getSaldo());
-			cuenta.extraer(10);
-			System.out.println("la cuenta ahora tiene el saldo" + cuenta.getSaldo());
-	*/}
+		int nro = 1234;
+		String cliente= "ivan delcuadri";
+		Cuenta cuenta = new Cuenta(nro, cliente);
+		cuenta.depositar(100);
+		cuenta.extraer(-10);
+		
+		}
 	
 	@Test
 	public void CrearCuentas(){
