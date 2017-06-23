835ca4728fe989e96851ff6060bc2628c752e51e
Your Name <you@example.com>
2017-06-23 13:50:25 -0300

    cuenta corriente y cuenta probadas

--- src/test/java/ivan/CuentaTest.java
+++ src/test/java/ivan/CuentaTest.java
@@ -5,21 +5,41 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class CuentaTest {
+	int nro = 1234;
+	String cliente= "ivan delcuadri";
+	Cuenta cuenta = new Cuenta(nro, cliente);
+	Cuenta c2  = new Cuenta(2,"pepe");
+	
+	
 
 	@Test
 	public void ExtraerNegativotest() {
-		int nro = 1234;
-		String cliente= "ivan delcuadri";
-		Cuenta cuenta = new Cuenta(nro, cliente);
+		
 		cuenta.depositar(100);
 		cuenta.extraer(-10);
+		assertEquals(100, cuenta.getSaldo());
+	
 		
 		}
 	
 	@Test
-	public void CrearCuentas(){
-		int nro = 1234;
-		String cliente= "ivan delcuadri";
-		Cuenta cuenta = new Cuenta(nro, cliente);
+	public void extraerBien(){
+		cuenta.depositar(100);
+		cuenta.extraer(50);
+		assertEquals(50, cuenta.getSaldo());
 	}
+	@Test
+	public void DepositarNegativos(){
+		c2.depositar(10);
+		c2.depositar(-1);
+		assertEquals(10, c2.getSaldo());
+	}
+	
+	@Test
+	public void DepositarTest(){
+		c2.depositar(10);
+		assertEquals(10, c2.getSaldo());
+	}
+	
+	
 }
