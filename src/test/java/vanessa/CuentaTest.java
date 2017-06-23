24a08058c210fc0ba3fbe8d9c729c203ced901e6
Vanessa <vballeste18@gmail.com>
2017-06-23 11:39:21 -0300

    Metodos nuevos para giro descubierto en cuenta corriente, creados test para cuentas corrientes

--- src/test/java/vanessa/CuentaTest.java
+++ src/test/java/vanessa/CuentaTest.java
@@ -8,6 +8,7 @@ import vanessa.Cuenta;
 
 public class CuentaTest {
 
+	/*Cuentas */
 	@Test
 	public void testDepositosCuenta() {
 		Cuenta c = new Cuenta(1, "Vanessa Balleste");
@@ -27,6 +28,75 @@ public class CuentaTest {
 		boolean extraer = c.extraer(60);
 		assertFalse(extraer);
 		assertEquals(50, c.getSaldo());
+		
+		assertFalse(c.extraer(-50));
+		assertEquals(50, c.getSaldo());
+	}
+	
+	
+	/*Cuentas Corrientes*/
+	@Test
+	public void testDepositosCuentaCorriente() {
+		CuentaCorriente c = new CuentaCorriente(1, "Vanessa Balleste", 100);
+		boolean depositar = c.depositar(50);
+		assertTrue(depositar);
+		assertEquals(50, c.getSaldo());
+		
+	}
+	
+	@Test
+	public void testExtraerCuentaCorriente() {
+		CuentaCorriente c = new CuentaCorriente(1, "Vanessa Balleste", 100);
+		boolean extraer = c.extraer(50);
+		
+		/*Extraer usando giro descubierto, queda saldo negativo*/
+		assertTrue(extraer);
+		assertEquals(-50, c.getSaldo());
+		assertEquals(50, c.getMontoDisponibleGiroDescubierto());
+		
+		/*Extraer monto negativo*/
+		extraer = c.extraer(-50);
+		assertFalse(extraer);
+			
+		/*Extraer monto mayor al disponible*/
+		extraer = c.extraer(500);
+		
+	}
+	
+	@Test
+	public void tesDepositarCuentaCorriente() {
+		CuentaCorriente c = new CuentaCorriente(1, "Vanessa Balleste", 100);
+		boolean depositar = c.depositar(50);
+		assertTrue(depositar);
+		assertEquals(50, c.getSaldo());
+		
+		
+	}
+	
+	@Test
+	public void tesDepositarCuentaCorrienteSaldoNeg() {
+		CuentaCorriente c = new CuentaCorriente(1, "Vanessa Balleste", 50);
+		boolean depositar = c.depositar(50);
+		assertTrue(depositar);
+		assertEquals(50, c.getSaldo());
+	}
+	
+	@Test
+	public void testGiroDescubierto() {
+		CuentaCorriente c = new CuentaCorriente(1, "Vanessa Balleste", 100);
+		assertEquals(100, c.giroEnDescubiertoHabilidado());
+		c.depositar(50);
+		assertEquals(100, c.giroEnDescubiertoHabilidado());
+		
+		/*Descontar de giro en descubierto, saldo negativo*/
+		c.extraer(60);
+		assertEquals(90, c.getMontoDisponibleGiroDescubierto());
+		assertEquals(-10, c.getSaldo());
+		
+		/*Extraer monto mayor al saldo disponible luego de haber 
+		 * quedado en saldo negativo, haciendo uso de giro descubierto*/
+		assertFalse(c.extraer(100));
+		
 	}
 		
 	
