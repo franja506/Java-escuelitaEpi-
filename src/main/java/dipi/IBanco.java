2697d58768181eba10b0ca295465698b8ab978f0
Nicolas D'Ippolito <ndippolito@dc.uba.ar>
2017-06-27 14:13:44 -0300

    mas cambios de interfaces

--- src/main/java/dipi/IBanco.java
+++ src/main/java/dipi/IBanco.java
@@ -3,6 +3,7 @@ package dipi;
 import java.util.Set;
 
 public interface IBanco {
+	public void agregarCliente(ICliente cliente);
 	public Set<ICuenta> getCuentas();
 	public Set<ICliente> getClientes();
 	public String getNombre();
--- src/main/java/dipi/ICliente.java
+++ src/main/java/dipi/ICliente.java
@@ -3,6 +3,7 @@ package dipi;
 import java.util.Set;
 
 public interface ICliente {
+	public void agregarCuenta(ICuenta cuenta);
 	public Set<ICuenta> getCuentas();
 	public String getNombre();
 }
