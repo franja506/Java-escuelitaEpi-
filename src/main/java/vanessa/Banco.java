fa257865d5b15a6d37b21c8a838a0e69fb0fe907
Vanessa Balleste <vballeste18@gmail.com>
2017-06-26 22:43:40 -0300

    clase banco y cliente actualizada

--- src/main/java/vanessa/Banco.java
+++ src/main/java/vanessa/Banco.java
@@ -1,14 +1,53 @@
 package vanessa;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.Map;
 
-import dipi.ICuenta;
 import vanessa.Cliente;
 
 public class Banco {
 	int id;
 	String nombreBanco;
-	List<ICuenta> cuentas;
-	List<Cliente> clientes;
+	Map<Long, Cuenta> cuentas;
+	Map<Long, CuentaCorriente> cuentasCorriente;
+	Map<Integer,Cliente> clientes;
+
+	public Banco(int id, String nombreBanco){
+		this.id = id;
+		this.nombreBanco = nombreBanco;
+		this.cuentas = new HashMap<Long, Cuenta>();
+		this.cuentasCorriente = new HashMap<Long,CuentaCorriente>();
+		this.clientes = new HashMap<Integer, Cliente>();
+	}
+	
+	public int getId(){
+		return this.id;
+	}
+	
+	public String getNombreBanco(){
+		return this.nombreBanco;
+	}
+	
+	public Map<Long, Cuenta> getCuentas(){
+		return this.cuentas;
+	}
+	
+	public Map<Long, CuentaCorriente> getCuentasCorriente(){
+		return this.cuentasCorriente;
+	}
+	
+	public Map<Integer, Cliente> getCuentasCliente(){
+		return this.clientes;
+	}
+	
+	public boolean removeCliente(int id){
+		if (this.clientes.containsKey(id)){
+			this.clientes.remove(id);
+			return true;
+		}else
+			return false;
+	}
+	
+	
 
 }
--- src/main/java/vanessa/Cliente.java
+++ src/main/java/vanessa/Cliente.java
@@ -5,6 +5,8 @@ import dipi.ICuenta;
 
 public class Cliente {
 	int id;
+	int cantidadCuentas;
+	int cantidadCuentasCorriente;
 	String nombreCliente;
 	Map<Long, Cuenta> cuentas;
 	Map<Long, CuentaCorriente> cuentasCorriente;
@@ -14,6 +16,15 @@ public class Cliente {
 		this.nombreCliente = nombre;
 		this.cuentas = new HashMap<Long, Cuenta>();
 	}
+	
+	public int getId(){
+		return this.id;
+	}
+	
+	public String getNombreCliente(){
+		return this.nombreCliente;
+	}
+	
 	
 	/*Cuentas generales*/
 	public Map<Long,Cuenta> getCuentas(){
@@ -48,6 +59,7 @@ public class Cliente {
 			return false;
 	}
 	
+	/*Depositar en cuenta/cuenta corriente dado un id cuenta y un monto*/
 	public boolean depositar(long idCuenta, long monto){
 		try{
 			return this.getCuenta(idCuenta).depositar(monto);	
@@ -56,8 +68,42 @@ public class Cliente {
 		}
 	}
 	
+	/*Agregar una cuenta a un cliente*/
+	public boolean agregarCuenta(Cuenta cuenta) throws Exception{
+		if (cuenta != null ){
+			if(this.cuentas.size() <= this.cuentasCorriente.size()){
+				throw new Exception("Operación no permitida. Agregue una Cuenta corriente nueva");
+			}else{
+				long k = cuenta.getId();
+				this.cuentas.put(k, cuenta);
+				return true;
+			}
+		}else{
+			return false;
+		}
+	}
 	
+	/*Agregar  una cuenta Corriente a un cliente*/
+	public boolean agregarCuentaCorriente(CuentaCorriente cuenta) throws Exception{
+		if(cuenta != null){
+				long k = cuenta.getId();
+				this.cuentasCorriente.put(k, cuenta);
+				return true;
+			}else{
+				return false;
+			}
+	}
 	
-	
-
+	/*Extraer monto de una cuenta/ cuenta corriente*/
+	public boolean extraerMontoCuenta(long numCuenta, long montoAExtraer){
+		try {
+			ICuenta cuenta = this.getCuenta(numCuenta);
+			return cuenta.extraer(montoAExtraer);
+		} catch (Exception e) {
+			e.getMessage();
+			return false;
+		}
+		
+	}
+		
 }
